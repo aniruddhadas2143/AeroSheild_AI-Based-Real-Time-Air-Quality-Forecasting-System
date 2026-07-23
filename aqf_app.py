74c68f2880a9dcd2c75e0beb4e3a99bfad5e1212
Aniruddha Das <130400454+aniruddhadas2143@users.noreply.github.com>
2026-07-23 15:42:27 +0530

    Update aqf_app.py

--- aqf_app.py
+++ aqf_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1fnSC3cZfeTLt02mRGF_qqcUSmg0HzlNO
 """
 
-!pip install streamlit joblib tensorflow scikit-learn pandas numpy
 !npm install -g localtunnel
 
 import streamlit as st
